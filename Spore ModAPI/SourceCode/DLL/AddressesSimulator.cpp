26ad6bc1e022da05ee7fc1888dd1389ef262ca21
0KepOnline <78259495+0KepOnline@users.noreply.github.com>
2026-07-11 00:01:15 +0300

    Add `Simulator::COMSerializer` & `Simulator::SerializerDatabase`

--- Spore ModAPI/SourceCode/DLL/AddressesSimulator.cpp
+++ Spore ModAPI/SourceCode/DLL/AddressesSimulator.cpp
@@ -672,6 +672,57 @@ namespace Simulator
 		DefineAddress(AttributesToXml, SelectAddress(0x695D40, 0x695B20));
 	}
 
+	namespace Addresses(COMSerializer)
+	{
+		DefineAddress(Open, SelectAddress(0x6902d0, 0x69dfc0));
+		DefineAddress(Close, SelectAddress(0x691910, 0x69f940));
+		DefineAddress(LoadClassObjects, SelectAddress(0x691240, 0x69f1d0));
+		DefineAddress(SaveClassObjects, SelectAddress(0x691af0, 0x69fb90));
+		DefineAddress(Write, SelectAddress(0x692440, 0x6a05f0));
+		DefineAddress(Read, SelectAddress(0x6924f0, 0x6a06a0));
+
+		DefineAddress(GetCRC, SelectAddress(0x690d40, 0x69ec20));
+		DefineAddress(reset, SelectAddress(0x6911d0, 0x69f160));
+		DefineAddress(onSetSPSerializable, SelectAddress(0x691ff0, 0x6a0110));
+		DefineAddress(onGetSPSerializable, SelectAddress(0x690bb0, 0x69e9e0));
+		DefineAddress(loadSingleObject, SelectAddress(0x6921e0, 0x6a0360));
+	}
+
+	namespace Addresses(SerializerDatabase)
+	{
+		DefineAddress(AsDatabase, SelectAddress(0x8fac50, 0x7f3190));
+		DefineAddress(GetCOMSerializer, SelectAddress(0x5a6210, 0x985d70));
+		DefineAddress(OpenReadStream, SelectAddress(0x690640, 0x69e3a0));
+		DefineAddress(CloseReadStream, SelectAddress(0x68f6e0, 0x69d2c0));
+		DefineAddress(LoadClassObjects, SelectAddress(0x690190, 0x69de80));
+		DefineAddress(OpenWriteStream, SelectAddress(0x6906d0, 0x69e470));
+		DefineAddress(CloseWriteStream, SelectAddress(0x68f6e0, 0x69d2c0));
+		DefineAddress(SaveClassObjects, SelectAddress(0x6901b0, 0x69dea0));
+		DefineAddress(HasKey, SelectAddress(0x6907d0, 0x69e5d0));
+		DefineAddress(GetPercentageCompletion, SelectAddress(0x109ffe0, 0xc06490));
+		DefineAddress(SetPercentageCompletion, SelectAddress(0x68f710, 0x69d2f0));
+		DefineAddress(OpenAsSerializer, SelectAddress(0x690230, 0x69df20));
+
+		DefineAddress(Initialize, SelectAddress(0x6901d0, 0x69dec0));
+		DefineAddress(Dispose, SelectAddress(0x6901e0, 0x69ded0));
+		DefineAddress(GetDatabaseType, SelectAddress(0x6900a0, 0x69dd90));
+		DefineAddress(GetRefCount, SelectAddress(0x68f800, 0x69d3f0));
+		DefineAddress(Lock, SelectAddress(0x6900b0, 0x69dda0));
+		DefineAddress(Open, SelectAddress(0x6900c0, 0x69ddb0));
+		DefineAddress(Close, SelectAddress(0x6902a0, 0x69df90));
+		DefineAddress(GetAccessFlags, SelectAddress(0x6900d0, 0x69ddc0));
+		DefineAddress(Flush, SelectAddress(0x6900f0, 0x69dde0));
+		DefineAddress(GetLocation, SelectAddress(0x690100, 0x69ddf0));
+		DefineAddress(SetLocation, SelectAddress(0x690110, 0x69de00));
+		DefineAddress(GetKeyList, SelectAddress(0x690120, 0x69de10));
+		DefineAddress(OpenRecord, SelectAddress(0x690130, 0x69de20));
+		DefineAddress(GetOpenCount, SelectAddress(0x690140, 0x69de30));
+		DefineAddress(CloseRecord, SelectAddress(0x690150, 0x69de40));
+		DefineAddress(DeleteRecord, SelectAddress(0x690160, 0x69de50));
+		DefineAddress(Attach, SelectAddress(0x690170, 0x69de60));
+		DefineAddress(GetAllocator, SelectAddress(0x690180, 0x69de70));
+	}
+
 	namespace Addresses(cGameData)
 	{
 		DefineAddress(Write, SelectAddress(0xB18430, 0xB184D0));
--- Spore ModAPI/SourceCode/Simulator/Serialization.cpp
+++ Spore ModAPI/SourceCode/Simulator/Serialization.cpp
@@ -7,7 +7,7 @@ namespace Simulator
 		, pAttributes(_attributes)
 		, id(classID)
 		, count(0)
-		, field_A04(0)
+		, countSerialized(0)
 	{
 		Attribute* ptr = _attributes;
 		while (ptr->readFunction && ptr->writeFunction && count < 128) {
@@ -44,5 +44,165 @@ namespace Simulator
 	{
 
 	}
+
+#ifndef MODAPI_DLL_EXPORT
+
+	ICOMSerializer::~ICOMSerializer() = default;
+
+	COMSerializer::COMSerializer(ISerializerDatabase* pDatabase)
+		: mbBusyWritingClassObjects(false)
+		, mClassIDMapForWriting()
+		, mClassIDMap()
+		, mTotalSerializableCount(0)
+		, mbClassObjectsLoaded(false)
+		, mbClassObjectsSaved(false)
+		, mpDatabase(nullptr)
+		, mCRCDataBuffer()
+	{
+		reset();
+		mpDatabase = pDatabase;
+	}
+
+	COMSerializer::~COMSerializer() = default;
+
+	int COMSerializer::AddRef()
+	{
+		return DefaultRefCounted::AddRef();
+	}
+
+	int COMSerializer::Release()
+	{
+		return DefaultRefCounted::Release();
+	}
+
+	void* COMSerializer::Cast(uint32_t type) const
+	{
+		CLASS_CAST(ICOMSerializer);
+		return nullptr;
+	}
+
+	auto_METHOD_VIRTUAL(COMSerializer, ICOMSerializer, bool, Open,
+		Args(ISerializerDatabase* pDatabase),
+		Args(pDatabase));
+	auto_METHOD_VIRTUAL_(COMSerializer, ICOMSerializer, bool, Close);
+	auto_METHOD_VIRTUAL(COMSerializer, ICOMSerializer, bool, LoadClassObjects,
+		Args(uint32_t clientVersion),
+		Args(clientVersion));
+	auto_METHOD_VIRTUAL_(COMSerializer, ICOMSerializer, bool, SaveClassObjects);
+	auto_METHOD_VIRTUAL(COMSerializer, ICOMSerializer, bool, Write,
+		Args(ISerializerWriteStream& oSPStream, const ISimulatorSerializable* pSerializable),
+		Args(oSPStream, pSerializable));
+	auto_METHOD_VIRTUAL(COMSerializer, ICOMSerializer, bool, Read,
+		Args(ISerializerReadStream& iSPStream, const ISimulatorSerializable** ppSerializable, bool bLoadImmediately),
+		Args(iSPStream, ppSerializable, bLoadImmediately));
+
+	auto_METHOD(COMSerializer, bool, GetCRC,
+		Args(Resource::IRecord* pRecord, uint32_t nCRCDataLength, uint32_t& nCRC),
+		Args(pRecord, nCRCDataLength, nCRC));
+	auto_METHOD_VOID_(COMSerializer, reset);
+	auto_METHOD(COMSerializer, bool, onSetSPSerializable,
+		Args(const ISimulatorSerializable* pSerializable, uint32_t instanceID, uint32_t classID),
+		Args(pSerializable, instanceID, classID));
+	auto_METHOD(COMSerializer, bool, onGetSPSerializable,
+		Args(ISimulatorSerializable** ppSerializable, uint32_t instanceID, uint32_t classID),
+		Args(ppSerializable, instanceID, classID));
+	auto_METHOD(COMSerializer, bool, loadSingleObject,
+		Args(ISimulatorSerializable* pSerializable, uint32_t instanceID, uint32_t classID),
+		Args(pSerializable, instanceID, classID));
+
+	SerializerDatabase::SerializerDatabase(Database* pDatabase)
+		: mpDatabase(pDatabase)
+		, mpCOMSerializer(new COMSerializer(this))
+		, mfPercentageCompletion0To100(0.0f)
+	{
+
+	}
+
+	SerializerDatabase::~SerializerDatabase() = default;
+
+	int SerializerDatabase::AddRef()
+	{
+		return Database::AddRef();
+	}
+
+	int SerializerDatabase::Release()
+	{
+		return Database::Release();
+	}
+
+	void* SerializerDatabase::Cast(uint32_t type) const
+	{
+		CLASS_CAST(ISerializerDatabase);
+		return nullptr;
+	}
+
+	auto_METHOD_VIRTUAL_(SerializerDatabase, ISerializerDatabase, Resource::Database*, AsDatabase);
+	auto_METHOD_VIRTUAL_(SerializerDatabase, ISerializerDatabase, ICOMSerializer*, GetCOMSerializer);
+	auto_METHOD_VIRTUAL(SerializerDatabase, ISerializerDatabase, bool, OpenReadStream,
+		Args(const ResourceKey& key, ISerializerReadStream** ppStream),
+		Args(key, ppStream));
+	auto_METHOD_VIRTUAL(SerializerDatabase, ISerializerDatabase, bool, CloseReadStream,
+		Args(ISerializerReadStream* pStream),
+		Args(pStream));
+	auto_METHOD_VIRTUAL(SerializerDatabase, ISerializerDatabase, bool, LoadClassObjects,
+		Args(uint32_t version),
+		Args(version));
+	auto_METHOD_VIRTUAL(SerializerDatabase, ISerializerDatabase, bool, OpenWriteStream,
+		Args(const ResourceKey& key, ISerializerWriteStream** ppStream, bool bTruncate),
+		Args(key, ppStream, bTruncate));
+	auto_METHOD_VIRTUAL(SerializerDatabase, ISerializerDatabase, bool, CloseWriteStream,
+		Args(ISerializerWriteStream* pStream),
+		Args(pStream));
+	auto_METHOD_VIRTUAL_(SerializerDatabase, ISerializerDatabase, bool, SaveClassObjects);
+	auto_METHOD_VIRTUAL_const(SerializerDatabase, ISerializerDatabase, bool, HasKey,
+		Args(const ResourceKey& key),
+		Args(key));
+	auto_METHOD_VIRTUAL_const_(SerializerDatabase, ISerializerDatabase, float, GetPercentageCompletion);
+	auto_METHOD_VIRTUAL_VOID(SerializerDatabase, ISerializerDatabase, SetPercentageCompletion,
+		Args(float fPercentageCompletion0To100),
+		Args(fPercentageCompletion0To100));
+
+	auto_METHOD_VIRTUAL_(SerializerDatabase, Resource::Database, bool, Initialize);
+	auto_METHOD_VIRTUAL_(SerializerDatabase, Resource::Database, bool, Dispose);
+	auto_METHOD_VIRTUAL_const_(SerializerDatabase, Resource::Database, uint32_t, GetDatabaseType);
+	auto_METHOD_VIRTUAL_const_(SerializerDatabase, Resource::Database, int, GetRefCount);
+	auto_METHOD_VIRTUAL_VOID(SerializerDatabase, Resource::Database, Lock,
+		Args(bool lock),
+		Args(lock));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, bool, Open,
+		Args(IO::AccessFlags desiredAccess, IO::CD createDisposition, bool bAutoOpen),
+		Args(desiredAccess, createDisposition, bAutoOpen));
+	auto_METHOD_VIRTUAL_(SerializerDatabase, Resource::Database, bool, Close);
+	auto_METHOD_VIRTUAL_const_(SerializerDatabase, Resource::Database, IO::AccessFlags, GetAccessFlags);
+	auto_METHOD_VIRTUAL_(SerializerDatabase, Resource::Database, bool, Flush);
+	auto_METHOD_VIRTUAL_const_(SerializerDatabase, Resource::Database, const char16_t*, GetLocation);
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, bool, SetLocation,
+		Args(const char16_t* path),
+		Args(path));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, size_t, GetKeyList,
+		Args(eastl::vector<ResourceKey>& dstVector, Resource::IKeyFilter* filter),
+		Args(dstVector, filter));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, bool, OpenRecord,
+		Args(const ResourceKey& name, Resource::IRecord** ppDst, IO::AccessFlags desiredAccess, IO::CD createDisposition, bool arg_10, Resource::RecordInfo* pDstInfo),
+		Args(name, ppDst, desiredAccess, createDisposition, arg_10, pDstInfo));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, int, GetOpenCount,
+		Args(const ResourceKey& key),
+		Args(key));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, bool, CloseRecord,
+		Args(Resource::IRecord* pRecord),
+		Args(pRecord));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, bool, DeleteRecord,
+		Args(const ResourceKey& name),
+		Args(name));
+	auto_METHOD_VIRTUAL(SerializerDatabase, Resource::Database, bool, Attach,
+		Args(bool arg_0, Resource::IResourceManager* pResourceMan, bool arg_8),
+		Args(arg_0, pResourceMan, arg_8));
+	auto_METHOD_VIRTUAL_const_(SerializerDatabase, Resource::Database, ICoreAllocator*, GetAllocator);
+
+	auto_METHOD_VIRTUAL(SerializerDatabase, SerializerDatabase, bool, OpenAsSerializer,
+		Args(bool bOpenForReading, bool bOpenForWriting),
+		Args(bOpenForReading, bOpenForWriting));
+
+#endif
 
 }
